baad6624a952382a8d7c1f1aaf046906ae718114
shengjunyang <1150969549@qq.com>
2019-03-11 16:46:31 +0800

    add config

---
+++ src/main/java/com/sjy/springboot/WebConfig.java
@@ -0,0 +1,13 @@
+package com.sjy.springboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author sjy on 2019/3/11
+ * @version 1.0
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+}
